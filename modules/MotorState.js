00d4c7e88d4d33aa3d14246e9097cae9bdb0b116
Pranav <82858629+Pranav-A-S@users.noreply.github.com>
2023-11-30 10:21:28 +0530

    Changes in Motor State Page

--- modules/MotorState.js
+++ modules/MotorState.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 import { useNavigation } from '@react-navigation/native';
 
 const MotorState = () => {
-  const [selectedBlock, setSelectedBlock] = useState('Select Block');
   const [isOn, setIsOn] = useState(false);
+  const [tableData, setTableData] = useState([
+    { tankType: 'OH T', waterLevel: '', motorStatus: '' },
+    { tankType: 'OH T', waterLevel: '', motorStatus: '' },
+    { tankType: 'UG S', waterLevel: '', motorStatus: '' },
+  ]);
+
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -29,36 +33,31 @@ const MotorState = () => {
   };
 
   const handleChangeButtonPress = () => {
-    if (selectedBlock === 'Select Block') {
-      // Show alert or handle accordingly
-    } else {
-      // Handle CHANGE button press using selectedBlock
-    }
+    // Handle CHANGE button press
+    const motorStatus = isOn ? 'ON' : 'OFF';
+    const updatedTableData = tableData.map((row) => ({ ...row, motorStatus }));
+    setTableData(updatedTableData);
   };
 
   return (
     <View style={styles.container}>
-      <Picker
-        selectedValue={selectedBlock}
-        onValueChange={(itemValue) => setSelectedBlock(itemValue)}
-        style={styles.picker}
-      >
-        <Picker.Item label="Select Block" value="Select Block" />
-        <Picker.Item label="Vasista Bhavanam" value="Vasista Bhavanam" />
-        <Picker.Item label="Gautama Bhavanam" value="Gautama Bhavanam" />
-        <Picker.Item label="Agastya Bhavanam" value="Agastya Bhavanam" />
-      </Picker>
       <View style={styles.motorOptions}>
         <Text style={styles.motorOptionsText}>Motor Options</Text>
         <TouchableOpacity
           onPress={handleOffButtonPress}
-          style={[styles.button, { backgroundColor: isOn ? 'transparent' : 'brown' }]}
+          style={[
+            styles.button,
+            { backgroundColor: isOn ? 'lightgrey' : 'brown' },
+          ]}
         >
           <Text style={{ color: isOn ? 'brown' : 'white', fontSize: 16 }}>OFF</Text>
         </TouchableOpacity>
         <TouchableOpacity
           onPress={handleOnButtonPress}
-          style={[styles.button, { backgroundColor: isOn ? 'brown' : 'transparent' }]}
+          style={[
+            styles.button,
+            { backgroundColor: isOn ? 'brown' : 'lightgrey' },
+          ]}
         >
           <Text style={{ color: isOn ? 'white' : 'brown', fontSize: 16 }}>ON</Text>
         </TouchableOpacity>
@@ -68,6 +67,27 @@ const MotorState = () => {
         >
           <Text style={styles.buttonText}>CHANGE</Text>
         </TouchableOpacity>
+      </View>
+
+      {/* Live Tank Data Table */}
+      <View style={styles.liveTankDataTable}>
+        <Text style={styles.liveTankDataTitle}>Live Tank Data</Text>
+        <View style={styles.table}>
+          <View style={styles.tableRow}>
+            <Text style={styles.tableHeader}>Tank Type</Text>
+            <Text style={styles.tableHeader}>Water Level</Text>
+            <Text style={styles.tableHeader}>Motor Status</Text>
+          </View>
+
+          {/* Rows */}
+          {tableData.map((row, index) => (
+            <View key={index} style={styles.tableRow}>
+              <Text style={styles.tableCell}>{row.tankType}</Text>
+              <Text style={styles.tableCell}>{row.waterLevel}</Text>
+              <Text style={styles.tableCell}>{row.motorStatus}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </View>
   );
@@ -79,10 +99,6 @@ const styles = StyleSheet.create({
     justifyContent: 'top',
     padding: 16,
   },
-  picker: {
-    height: 50,
-    marginBottom: 20,
-  },
   motorOptions: {
     alignItems: 'center',
   },
@@ -90,9 +106,10 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
+    marginTop: 10,
   },
   button: {
-    width: 100,
+    width: 180, // Same width for both buttons
     height: 40,
     justifyContent: 'center',
     alignItems: 'center',
@@ -104,13 +121,53 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   changeButton: {
-    width: 100,
+    width: 180, // Same width for both buttons
     height: 40,
     backgroundColor: 'black',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 5,
     marginTop: 10,
+    marginBottom: 20,
+  },
+  liveTankDataTable: {
+    marginTop: 20,
+  },
+  liveTankDataTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    alignSelf: 'center',
+  },
+  table: {
+    borderWidth: 1,
+    borderColor: 'black',
+    marginTop: 10,
+    marginRight: 10,
+    marginLeft: 10,
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    borderBottomWidth: 1,
+    borderColor: 'black',
+    padding: 5,
+  },
+  tableHeader: {
+    fontWeight: 'bold',
+    flex: 1,
+    textAlign: 'center',
+  },
+  tableCell: {
+    flex: 1,
+    padding: 5,
+    textAlign: 'center',
+  },
+  tableCellSubtitle: {
+    flex: 1,
+    padding: 5,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
